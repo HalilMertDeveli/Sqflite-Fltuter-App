389c2bd310f8ba9bf6f28efa8ecf58a94a327d75
HMDSS <halilmertdeveliii@gmail.com>
2024-06-29 17:45:27 +0300

    controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(home: HomePage()));
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
 }
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  final formKey = GlobalKey<FormState>();
-
-  String dogName = "";
-
-  int dogAge = 0;
+class HomePage extends StatelessWidget{
+  final textController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("SQlite apps"),
+        title: const Text("it is app bar"),
       ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-        child: Form(
-          key: formKey,
-          child: Column(
-            children: [
-              SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                onChanged: (value) {
-                  setState(() {
-                    dogName = value;
-                  });
-                },
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                onChanged: (value) {
-                  setState(() {
-                    dogAge = int.parse(value);
-                  });
-                },
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    print(dogName);
-                    print(dogAge);
-                  },
-                  child: Row(
-                    children: [Text("Save"), Icon(Icons.save)],
-                  ))
-            ],
+      body: Column(
+        children: [
+          TextField(
+            controller: textController,
           ),
-        ),
+          SizedBox(
+            height: 30,
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.save),
+        onPressed: (){
+            print(textController.text);
+        },
       ),
     );
   }
+
 }
